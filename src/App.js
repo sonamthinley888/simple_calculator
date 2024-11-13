ed07a30bd7303e27428a8d260ed387f15700f910
Sonam Thinley <sonamthinley888@gmail.com>
2024-11-14 00:10:53 +1100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+
+import React, { useReducer } from 'react';
+import "./Styles.css"
+import DigitButton from "./DigitButton"
+import OperationButton from './OperationButton';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate',
+}
+
+// Initial state
+const initialState = {
+  currentOperand: '',
+  previousOperand: '',
+  operation: null,
+};
+
+function reducer(state, {type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+  }
+}
+
+function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+        
+      </div>  
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <OperationButton operation ="÷" dispatch={dispatch}/>
+     
+      <DigitButton digit ="1" dispatch={dispatch} />
+      <DigitButton digit ="2" dispatch={dispatch} />
+      <DigitButton digit ="3" dispatch={dispatch} />
+      <OperationButton operation ="*" dispatch={dispatch}/>
+
+      <DigitButton digit ="4" dispatch={dispatch} />
+      <DigitButton digit ="5" dispatch={dispatch} />
+      <DigitButton digit ="6" dispatch={dispatch} />
+      <OperationButton operation ="+" dispatch={dispatch}/>
+
+      <DigitButton digit ="7" dispatch={dispatch} />
+      <DigitButton digit ="8" dispatch={dispatch} />
+      <DigitButton digit ="9" dispatch={dispatch} />
+      <OperationButton operation ="-" dispatch={dispatch}/>
+
+      <DigitButton digit ="0" dispatch={dispatch} />
+      <DigitButton digit ="." dispatch={dispatch} />
+      <button className="span-two">=</button>
+    </div>  
+    
+  )
+}
+
+export default App
